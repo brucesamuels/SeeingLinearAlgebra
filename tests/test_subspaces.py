740eecd59bfce55f60a7cb8b352b76e0e5e4582a
Bruce Samuels <Bruce.Samuels@gmail.com>
2026-07-11 15:50:17 -0400

    Engine v0.2: Implement dimension-independent vectors and subspaces; pass smoke tests

--- tests/test_subspaces.py
+++ tests/test_subspaces.py
@@ -1,4 +1,4 @@
-from manim import Create, FadeIn, MathTex
+from manim import Create, FadeIn, MathTex, UR
 from engine.scene_tools import SeeingScene
 from engine.vectors import Vector
 from engine.subspaces import Subspace
@@ -15,7 +15,7 @@ class SubspaceEngineSmokeTest(SeeingScene):
         b = VectorVisual2D(v2, r"\mathbf v_2", VECTOR_2)
         self.play(Create(plane), Create(a.arrow), FadeIn(a.label))
         self.play(Create(b.arrow), FadeIn(b.label))
-        self.play(FadeIn(MathTex(r"\operatorname{rank}=2", color=TEXT).to_corner("UR")))
+        self.play(FadeIn(MathTex(r"\operatorname{rank}=2", color=TEXT).to_corner(UR)))
         dep = Subspace([Vector([2,1]), Vector([4,2])])
         self.play(Create(SpanLineVisual(dep, 2.5, COMBINATION)))
         self.wait(1)
